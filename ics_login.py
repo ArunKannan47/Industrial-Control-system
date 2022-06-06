676bfa562442cebe5ff6abbaee93fb9b8e8b5a5d
Arun Kannan <63847723+ArunKannan47@users.noreply.github.com>
2022-06-06 14:40:00 +0530

    Add files via upload

---
+++ ics_login.py
@@ -0,0 +1,21 @@
+#!C:\Users\Arun Kannan\AppData\Local\Programs\Python\Python37\python.exe
+
+
+print("Content-type: text/html\r\n\r\n")
+print("<title>Industrial Control System</title>")  
+print("<style>h1{color: green; text-align: center}</style>")
+
+
+print("<html><body>")
+print("<h1> Login to Indutrial Control System </h1>")
+
+  
+
+  
+# Using HTML input and form method POST
+print("<form method='post' action='password_validation'>")
+print("<center><p>Username: <input type='text' name='name' /></center></p>")
+print("<center><p>Password: <input type='text' name='name' /></center></p>")
+print("<center><br><input type='submit' value='Login' /></center>")
+print("</form")
+print("</body></html>")
